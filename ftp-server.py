be7d396503c16c0e4b7cd877afe997fbd15fa50e
Jack Rosenhauer <jackrosenhauer@gmail.com>
2016-10-01 15:57:57 -0400

    accepts data from the control connection

--- ftp-server.py
+++ ftp-server.py
@@ -4,22 +4,33 @@ class ftp_server:
     #basic class/socket setup
     def __init__(self):
         # self.port = 7710
-        self.port = 80
+        self.port = 7710
         self.server_socket = socket.socket()
-        self.server_socket.bind(("localhost", 80))
+        self.server_socket.bind(("localhost", self.port))
         self.server_socket.listen(1)
 
-        #accept our command connection
-        conn, addr = self.server_socket.accept()
-        print (addr)
-        self.loop(conn, addr)
+        self.get_connection()
 
+    def get_connection(self):
+        conn, addr = self.server_socket.accept()
+        self.loop(conn, addr)
 
     #establishes connection from a client, need to setup to receive commands and act accordingly
     def loop(self, conn, addr):
         print ("accepted connection with: " + addr[0] + ":" + str(addr[1]))
         while True:
-            print ("true")
+            cmd = conn.recv(256)
+            if cmd:
+                if not cmd.endswith("\r\n"):
+                    print ("ERRORONIOUS CMD FROM CLIENT")
+                    continue
+                else:
+                    cmd = cmd.rstrip("\r\n")
+
+                print (len(cmd))
+                split = cmd.split(" ")
+                print (split)
+                print ("cmd: " + cmd)
 
 #kick it off
 ftp_server()
