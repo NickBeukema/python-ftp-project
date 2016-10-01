37e5bf60da30e50867338a23fa200f28a48ebee7
Jack Rosenhauer <jackrosenhauer@gmail.com>
2016-10-01 15:55:32 -0400

    parses command in lowercase, now uses a class format and added a dummy command 'hi' for testing

--- ftp-client.py
+++ ftp-client.py
@@ -1,87 +1,97 @@
 import socket
 from lib2to3.fixer_util import String
+class FtpClient:
+    def __init__(self):
+        self.ctrlSock = socket.socket()
 
-#CONNECT FUNCTION
-def connect(entry_array):
-    
-    #Make sure correct amount of parameters were passed
-    if len(entry_array) != 3:
-        print("Invalid command - CONNECT Parameters: <server name/IP address> <server port>")
-        return
-    
-    #Parse control port to integer value
-    try:
-        ctrlPort = int(entry_array[2])
-    except ValueError:
-        print("Invalid port number")
-        return
-    
-    #Establish control connection
-    ctrlSock = socket.socket()
-    try:
-#         ctrlSock.connect(("127.0.0.1", 80))
-        ctrlSock.connect((entry_array[1], ctrlPort))
-    except ConnectionRefusedError:
-        print("Connection refused - check port number")
-        return
-        
-    print("Connection established on port {}.".format(ctrlPort))
-    
-    #TODO: Data socket connection
-    
-        
-#LIST FUNCTION
-def list(entry_array):
-    
-    #Make sure correct amount of parameters were passed
-    if len(entry_array) != 1:
-        print("Invalid command - LIST requires no additional parameters")
-        return
-    
-    
-#RETRIEVE FUNCTION
-def retr(entry_array):
-    
-    #Make sure correct amount of parameters were passed
-    if len(entry_array) != 2:
-        print("Invalid command - RETR Parameters: <filename>")
-        return
-    
-    
-#STORE FUNCTION
-def stor(entry_array):
-    
-    #Make sure correct amount of parameters were passed
-    if len(entry_array) != 2:
-        print("Invalid command - STOR Parameters: <filename>")
-        return
-    
-    
-#QUIT FUNCTION
-def quit(entry_array):
-    
-    #Make sure correct amount of parameters were passed
-    if len(entry_array) != 1:
-        print("Invalid command - QUIT requires no additional parameters")
-        return 
+        #MAIN LOOP
+        ################
+        while True:
+            entry_array = raw_input("Please enter command: ").lower().split(" ")
+            if entry_array[0] == "connect":
+                self.connect(entry_array)
+            elif entry_array[0] == "list":
+                self.list(entry_array)
+            elif entry_array[0] == "retr":
+                self.retr(entry_array)
+            elif entry_array[0] == "stor":
+                self.stor(entry_array)
+            elif entry_array[0] == "quit":
+                self.quit(entry_array)
+            elif entry_array[0] == "hi":
+                self.hi()
+            else:
+                print("Unknown command: '" + entry_array[0] + "'")
+
+    #CONNECT FUNCTION
+    def connect(self, entry_array):
+
+        #Make sure correct amount of parameters were passed
+        if len(entry_array) != 3:
+            print("Invalid command - CONNECT Parameters: <server name/IP address> <server port>")
+            return
+
+        #Parse control port to integer value
+        try:
+            ctrlPort = int(entry_array[2])
+        except ValueError:
+            print("Invalid port number")
+            return
+
+        #Establish control connection
+        #ctrlSock = socket.socket()
+        try:
+    #         ctrlSock.connect(("127.0.0.1", 80))
+            self.ctrlSock.connect((entry_array[1], ctrlPort))
+        except ConnectionRefusedError:
+            print("Connection refused - check port number")
+            return
+
+        print("Connection established on port {}.".format(ctrlPort))
+
+        #TODO: Data socket connection
 
 
+    #LIST FUNCTION
+    def list(self, entry_array):
+
+        #Make sure correct amount of parameters were passed
+        if len(entry_array) != 1:
+            print("Invalid command - LIST requires no additional parameters")
+            return
 
 
-#MAIN LOOP
-################
-while True:
-    entry_array = input("Please enter command: ").split(" ")
-    
-    if entry_array[0] == "CONNECT":
-        connect(entry_array)
-    elif entry_array[0] == "LIST":
-        list(entry_array)
-    elif entry_array[0] == "RETR":
-        retr(entry_array)
-    elif entry_array[0] == "STOR":
-        stor(entry_array)
-    elif entry_array[0] == "QUIT":
-        quit(entry_array)
-    else:
-        print("Unknown command")
+    #RETRIEVE FUNCTION
+    def retr(self, entry_array):
+
+        #Make sure correct amount of parameters were passed
+        if len(entry_array) != 2:
+            print("Invalid command - RETR Parameters: <filename>")
+            return
+
+
+    #STORE FUNCTION
+    def stor(self, entry_array):
+
+        #Make sure correct amount of parameters were passed
+        if len(entry_array) != 2:
+            print("Invalid command - STOR Parameters: <filename>")
+            return
+
+
+    #QUIT FUNCTION
+    def quit(self, entry_array):
+
+        #Make sure correct amount of parameters were passed
+        if len(entry_array) != 1:
+            print("Invalid command - QUIT requires no additional parameters")
+            return
+        else:
+            self.ctrlSock.send("QUIT\r\n")
+            print ("Quitting")
+            exit()
+
+    def hi(self):
+        self.ctrlSock.sendall("cmd param1 param2")
+
+FtpClient()
