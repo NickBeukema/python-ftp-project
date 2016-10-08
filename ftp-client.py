9dc216632255c98112dc93836cfaff4960b4dd89
Jack Rosenhauer II <jackrosenhauer@gmail.com>
2016-10-08 11:50:06 -0400

    fixed spacing and uncommented server commands

--- ftp-client.py
+++ ftp-client.py
@@ -8,6 +8,7 @@ from lib2to3.fixer_util import String
 
 ftp_client_default_port = 7712
 
+
 # class ftp_data_thread(threading.Thread):
 #     def __init__(self, socket):
 #         self.socket = socket
@@ -30,205 +31,195 @@ ftp_client_default_port = 7712
 
 
 class ftp_client:
-    def __init__(self):
-        self.ctrlSock = socket.socket()
+  def __init__(self):
+    self.ctrlSock = socket.socket()
 
-        #MAIN LOOP
-        ################
-        while True:
-            entry_array = input("Please enter command: ").lower().split(" ")
-            if entry_array[0] == "connect":
-                self.connect(entry_array)
-            elif entry_array[0] == "list":
-                self.list(entry_array)
-            elif entry_array[0] == "retr":
-                self.retr(entry_array)
-            elif entry_array[0] == "stor":
-                self.stor(entry_array)
-            elif entry_array[0] == "quit":
-                self.quit(entry_array)
-            elif entry_array[0] == "hi":
-                self.hi()
-            else:
-                print("Unknown command: '" + entry_array[0] + "'")
+    # MAIN LOOP
+    ################
+    while True:
+      entry_array = input("Please enter command: ").lower().split(" ")
+      if entry_array[0] == "connect":
+        self.connect(entry_array)
+      elif entry_array[0] == "list":
+        self.list(entry_array)
+      elif entry_array[0] == "retr":
+        self.retr(entry_array)
+      elif entry_array[0] == "stor":
+        self.stor(entry_array)
+      elif entry_array[0] == "quit":
+        self.quit(entry_array)
+      elif entry_array[0] == "hi":
+        self.hi()
+      else:
+        print("Unknown command: '" + entry_array[0] + "'")
 
-    #CONNECT FUNCTION
-    def connect(self, entry_array):
+  # CONNECT FUNCTION
+  def connect(self, entry_array):
 
-        #Make sure correct amount of parameters were passed
-        if len(entry_array) != 3:
-            print("Invalid command - CONNECT Parameters: <server name/IP address> <server port>")
-            return
+    # Make sure correct amount of parameters were passed
+    if len(entry_array) != 3:
+      print("Invalid command - CONNECT Parameters: <server name/IP address> <server port>")
+      return
 
-        #Parse control port to integer value
-        try:
-            ctrlPort = int(entry_array[2])
-        except ValueError:
-            print("Invalid port number")
-            return
+    # Parse control port to integer value
+    try:
+      ctrlPort = int(entry_array[2])
+    except ValueError:
+      print("Invalid port number")
+      return
 
-        #Establish control connection
-        #ctrlSock = socket.socket()
-        try:
-#             self.ctrlSock.connect(("127.0.0.1", 7711))
-            self.ctrlSock.connect((entry_array[1], ctrlPort))
-            #after connection is established, we need to wait for the 220 response from the server "awaiting input"
-            self.get_response()
-        except ConnectionRefusedError:
-            print("Connection refused - check port number")
-            return
+    # Establish control connection
+    # ctrlSock = socket.socket()
+    try:
+      #             self.ctrlSock.connect(("127.0.0.1", 7711))
+      self.ctrlSock.connect((entry_array[1], ctrlPort))
+      # after connection is established, we need to wait for the 220 response from the server "awaiting input"
+      self.get_response()
+    except ConnectionRefusedError:
+      print("Connection refused - check port number")
+      return
 
-        print("Connection established on port {}.".format(ctrlPort))
+    print("Connection established on port {}.".format(ctrlPort))
 
+  # LIST FUNCTION
+  def list(self, entry_array):
 
+    # Make sure correct amount of parameters were passed
+    # Can we pass in a directory?
+    if len(entry_array) != 1:
+      print("Invalid command - LIST requires no additional parameters")
+      return
 
-    #LIST FUNCTION
-    def list(self, entry_array):
+    self.send("LIST")
 
-        #Make sure correct amount of parameters were passed
-        # Can we pass in a directory?
-        if len(entry_array) != 1:
-            print("Invalid command - LIST requires no additional parameters")
-            return
+    self.openDataPort()
 
-        self.send("LIST")
+    total_payload = ""
+    data = str(self.dataConn.recv(1024), "utf-8")
+    total_payload += data
 
-        self.openDataPort()
-
-        total_payload = ""
-        data = str(self.dataConn.recv(1024), "utf-8")
+    print("Receiving file list... \n")
+    while data:
+      data = self.dataConn.recv(1024)
+      if data:
         total_payload += data
 
-        print("Receiving file list... \n")
-        while data:
-          data = self.dataConn.recv(1024)
-          if data:
-            total_payload += data
+    # Handle Payload
+    print(total_payload)
 
-        # Handle Payload
-        print(total_payload)
+    # Close data port
+    self.closeDataPort()
+    return
 
-        # Close data port
-        self.closeDataPort()
+  # RETRIEVE FUNCTION
+  def retr(self, entry_array):
+
+    # Make sure correct amount of parameters were passed
+    if len(entry_array) != 2:
+      print("Invalid command - RETR Parameters: <filename>")
+      return
+
+    self.send("RETR " + entry_array[1])
+    self.openDataPort()
+
+    # Retrieve file from server
+    # f = open("file1.txt", "wb")
+    # data = self.dataConn.recv(1024)
+    # print("Retrieving file... \n")
+    # while data:
+    #   f.write(data)
+    #   data = self.dataConn.recv(1024)
+    #
+    # f.close()
+
+    self.closeDataPort()
+
+  # STORE FUNCTION
+  def stor(self, entry_array):
+
+    # Make sure correct amount of parameters were passed
+    if len(entry_array) != 2:
+      print("Invalid command - STOR Parameters: <filename>")
+      return
+
+    self.send("STOR " + entry_array[1])
+    self.openDataPort()
+
+    # Send file to server
+    # f = open("file1.txt", "wb")
+    # data = self.dataConn.recv(1024)
+    # print("Sending file... \n")
+    # while data:
+    #   f.write(data)
+    #   data = self.dataConn.recv(1024)
+    #
+    # f.close()
+
+    self.closeDataPort()
+
+  # QUIT FUNCTION
+  def quit(self, entry_array):
+
+    # Make sure correct amount of parameters were passed
+    if len(entry_array) != 1:
+      print("Invalid command - QUIT requires no additional parameters")
+      return
+    else:
+      self.send("QUIT")
+      print("Quitting")
+      # TODO: Should we exit program here or just end the connection with the server?
+      exit()
+
+  def get_response(self):
+    response = self.ctrlSock.recv(256, "utf-8")
+    self.parse_response(response)
+
+  def parse_response(self, response):
+    if (len(response) == 3):
+      response_code = response
+    else:
+      response_code = response[:3]
+      response_message = response[4:]
+
+    print("response code: " + response_code + ", message: " + response_message)
+
+  def hi(self):
+    self.send("cmd param1 param2")
+
+  def send(self, message, encoding="utf-8"):
+    self.ctrlSock.sendall(bytearray(message + "\r\n", encoding))
+
+  def openDataPort(self):
+    #       self.fct = ftp_data_thread(self.dataConn)
+    #       self.fct.start()
+    self.dataPort = 6548
+    self.dataSock = socket.socket()
+    self.dataSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    self.dataSock.bind(("127.0.0.1", self.dataPort))
+    self.dataSock.listen(1)
+
+    while True:
+      try:
+        print("Waiting for server to connect to data socket")
+        self.dataConn, addr = self.dataSock.accept()
+
+        # print ("Accepted data connection: " + addr[0] + ":" + str(addr[1]))
+
+        resp = str(self.ctrlSock.recv(256), "utf-8")
+        print(resp)
+
+        #                 self.fct = ftp_data_thread(self.dataConn)
+        #                 self.fct.start()
+
         return
+      except:
+        print("Unexpected error: ", sys.exc_info()[0])
+        self.dataSock.close()
+        exit()
 
-
-    #RETRIEVE FUNCTION
-    def retr(self, entry_array):
-
-        #Make sure correct amount of parameters were passed
-        if len(entry_array) != 2:
-            print("Invalid command - RETR Parameters: <filename>")
-            return
-
-        self.send("RETR " + entry_array[1])
-        self.openDataPort()
-
-        # Retrieve file from server
-        # f = open("file1.txt", "wb")
-        # data = self.dataConn.recv(1024)
-        # print("Retrieving file... \n")
-        # while data:
-        #   f.write(data)
-        #   data = self.dataConn.recv(1024)
-        #
-        # f.close()
-
-        self.closeDataPort()
-
-
-    #STORE FUNCTION
-    def stor(self, entry_array):
-
-        #Make sure correct amount of parameters were passed
-        if len(entry_array) != 2:
-            print("Invalid command - STOR Parameters: <filename>")
-            return
-
-        self.send("STOR " + entry_array[1])
-        self.openDataPort()
-
-        # Send file to server
-        # f = open("file1.txt", "wb")
-        # data = self.dataConn.recv(1024)
-        # print("Sending file... \n")
-        # while data:
-        #   f.write(data)
-        #   data = self.dataConn.recv(1024)
-        #
-        # f.close()
-
-        self.closeDataPort()
-
-
-    #QUIT FUNCTION
-    def quit(self, entry_array):
-
-        #Make sure correct amount of parameters were passed
-        if len(entry_array) != 1:
-            print("Invalid command - QUIT requires no additional parameters")
-            return
-        else:
-            self.send("QUIT")
-            print ("Quitting")
-            #TODO: Should we exit program here or just end the connection with the server?
-            exit()
-
-    def get_response(self):
-        response = self.ctrlSock.recv(256, "utf-8")
-        self.parse_response(response)
-
-    def parse_response(self, response):
-        if (len(response) == 3):
-            response_code = response
-        else:
-            response_code = response[:3]
-            response_message = response[4:]
-
-        print("response code: " + response_code + ", message: " + response_message)
-
-
-    def hi(self):
-        self.send("cmd param1 param2")
-
-
-    def send(self, message, encoding="utf-8"):
-        self.ctrlSock.sendall(bytearray(message + "\r\n", encoding))
-
-
-    def openDataPort(self):
-#       self.fct = ftp_data_thread(self.dataConn)
-#       self.fct.start()
-        self.dataPort = 6548
-        self.dataSock = socket.socket()
-        self.dataSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.dataSock.bind(("127.0.0.1", self.dataPort))
-        self.dataSock.listen(1)
-
-        while True:
-            try:
-                print ("Waiting for server to connect to data socket")
-                self.dataConn, addr = self.dataSock.accept()
-
-                # print ("Accepted data connection: " + addr[0] + ":" + str(addr[1]))
-
-                resp=str(self.ctrlSock.recv(256), "utf-8")
-                print(resp)
-
-#                 self.fct = ftp_data_thread(self.dataConn)
-#                 self.fct.start()
-
-                return
-            except:
-                print ("Unexpected error: ", sys.exc_info()[0])
-                self.dataSock.close()
-                exit()
-
-    def closeDataPort(self):
-      print("Done receiving")
-      self.dataConn.close()
-
-
+  def closeDataPort(self):
+    print("Done receiving")
+    self.dataConn.close()
 
 
 if __name__ == '__main__':
--- ftp-server.py
+++ ftp-server.py
@@ -10,154 +10,163 @@ ftp_client_default_port = 7712  # we can't use default ports on the same compute
 
 
 class ftp_data_thread(threading.Thread):
-    def __init__(self, socket, init=False):
-        # self.data_socket = socket.socket()
-        # self.current_dir = os.path.abspath("./ftp-files/")
-        threading.Thread.__init__(self)
 
-    def run(self):
-        print("running data thread")
+  def __init__(self, socket, init=False):
+    self.data_socket = socket.socket()
+    self.current_dir = os.path.abspath("./ftp-files/")
+    threading.Thread.__init__(self)
+
+  def run(self):
+    print("running data thread")
 
 
 class ftp_command_thread(threading.Thread):
-    def __init__(self, socket):
-        self.socket = socket
-        self.current_dir = os.path.abspath("./ftp-files/")
-        self.data_socket = None
-        threading.Thread.__init__(self)
+  def __init__(self, socket):
+    self.socket = socket
+    self.current_dir = os.path.abspath("./ftp-files/")
+    self.data_socket = None
+    threading.Thread.__init__(self)
 
-    def run(self):
-        while True:
-            self.socket.send("220 awaiting input")
+  def run(self):
+    while True:
+      self.socket.send("220 awaiting input")
 
-            # wait for commands
-            cmd = str(self.socket.recv(1024), "utf-8")
+      # wait for commands
+      cmd = str(self.socket.recv(1024), "utf-8")
 
-            if cmd:
-                if not cmd.endswith("\r\n"):
-                    print("ERRONEOUS CMD FROM CLIENT")
-                    print(cmd)
-                    continue
-                else:
-                    cmd = cmd.rstrip("\r\n")
-
-                split = cmd.lower().split(" ")
-
-                if split[0] == "quit":
-                    self.quit()
-                elif split[0] == "list":
-                    print("list")
-                    # if len(split) == 2:
-                    #     self.list(split[1])
-                    # else:
-                    #     self.list()
-                elif split[0] == "retr":
-                    print("retr")
-                # if len(split) == 2:
-                #     self.retr(split[1])
-                #   else:
-                #     #error, must supply filename
-                #     print ("retr error no filename")
-                elif split[0] == "stor":
-                    print("stor")
-                # if len(split) == 2:
-                #     self.stor(split[1])
-                #   else:
-                #     print ("stor error no filename")
-                else:
-                    print("Unhandled command: " + split[0])
-
-                print("cmd: " + cmd)
-
-    def send_ctrl_response(self, message, encoding="utf-8"):
-        self.socket.sendall(bytearray(message + "\r\n", encoding))
-
-    def open_data_socket(self):
-        if self.data_socket is None:
-            self.send_ctrl_response('150 About to open data connection.')
-            try:
-                self.data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.data_socket.connect(("127.0.0.1", 6548))
-                print("Data connection established.")
-            except:
-                self.send_ctrl_response('425 Can’t open data connection.')
+      if cmd:
+        if not cmd.endswith("\r\n"):
+          print("ERRONEOUS CMD FROM CLIENT")
+          print(cmd)
+          continue
         else:
-            self.send_ctrl_response('125 Data connection already open, transfer starting')
+          cmd = cmd.rstrip("\r\n")
 
-    def close_data_socket(self):
-        self.data_socket.close()
-        self.data_socket = None
-        self.send_ctrl_response('226 Closing data connection.  Requested file action successful.')
+        split = cmd.lower().split(" ")
 
-    def quit(self):
-        print("quitting")
+        if split[0] == "quit":
+          self.quit()
+        elif split[0] == "list":
+          print("list")
+          if len(split) == 2:
+              self.list(split[1])
+          else:
+              self.list(self.current_dir)
+        elif split[0] == "retr":
+          print("retr")
+          if len(split) == 2:
+            self.retr(split[1])
+          else:
+            # error, must supply filename
+            print("retr error no filename")
+        elif split[0] == "stor":
+          print("stor")
+          if len(split) == 2:
+            self.stor(split[1])
+          else:
+            print("stor error no filename")
+        else:
+          print("Unhandled command: " + split[0])
 
-    def list(self):
-        # Open data connection
-        self.open_data_socket()
+      print("cmd: " + cmd)
 
-        try:
-            files_in_dir = os.listdir(self.current_dir)
-            files_in_dir.sort()
-            file_string = ""
-            for file in files_in_dir:
-                file_string = file_string + file + "\n"
-            self.data_socket.sendall(bytearray(file_string + "\r\n", "utf-8"))
-        except:
-            self.send_ctrl_response('451 Requested action aborted: local error in processing.')
 
-        self.close_data_socket()
+  def send_ctrl_response(self, message, encoding="utf-8"):
+    self.socket.sendall(bytearray(message + "\r\n", encoding))
 
-    def retr(self, filename):
-        # Open data connection
-        self.openDataConn()
 
-        the_file = open(filename, 'rb')
-        data = the_file.read(1024)
+  def open_data_socket(self):
+    if self.data_socket is None:
+      self.send_ctrl_response('150 About to open data connection.')
+      try:
+        self.data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.data_socket.connect(("127.0.0.1", 6548))
+        print("Data connection established.")
+      except:
+        self.send_ctrl_response('425 Can’t open data connection.')
+    else:
+      self.send_ctrl_response('125 Data connection already open, transfer starting')
 
-        while data:
-            ## TODO: Send the file part
-            ## self.data_sock.send(data)
-            data = the_file.read(1024)
 
-        ## Can we send it all at once?
-        ## self.data_sock.send(the_file.read())
+  def close_data_socket(self):
+    self.data_socket.close()
+    self.data_socket = None
+    self.send_ctrl_response('226 Closing data connection.  Requested file action successful.')
 
-        the_file.close()
-        ## TODO: Close data connection
-        self.socket.send('226 Transfer complete.\r\n')
 
-    def stor(self, filename):
-        print("storing file " + filename)
+  def quit(self):
+    print("quitting")
+
+
+  def list(self, dir):
+    # Open data connection
+    self.open_data_socket()
+
+    try:
+      files_in_dir = os.listdir(self.current_dir)
+      files_in_dir.sort()
+      file_string = ""
+      for file in files_in_dir:
+        file_string = file_string + file + "\n"
+      self.data_socket.sendall(bytearray(file_string + "\r\n", "utf-8"))
+    except:
+      self.send_ctrl_response('451 Requested action aborted: local error in processing.')
+
+    self.close_data_socket()
+
+
+  def retr(self, filename):
+    # Open data connection
+    self.openDataConn()
+
+    the_file = open(filename, 'rb')
+    data = the_file.read(1024)
+
+    while data:
+      ## TODO: Send the file part
+      ## self.data_sock.send(data)
+      data = the_file.read(1024)
+
+    ## Can we send it all at once?
+    ## self.data_sock.send(the_file.read())
+
+    the_file.close()
+    ## TODO: Close data connection
+    self.socket.send('226 Transfer complete.\r\n')
+
+
+  def stor(self, filename):
+    print("storing file " + filename)
 
 
 class ftp_server:
-    # basic class/socket setup
-    def __init__(self):
-        # self.port = 7711
-        self.server_socket = socket.socket()
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server_socket.bind(("localhost", command_port))
+  # basic class/socket setup
+  def __init__(self):
+    # self.port = 7711
+    self.server_socket = socket.socket()
+    self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    self.server_socket.bind(("localhost", command_port))
 
-        # listen for 1 client
-        self.server_socket.listen(1)
+    # listen for 1 client
+    self.server_socket.listen(1)
 
-        while True:
-            try:
-                print("waiting for connection on " + self.server_socket.getsockname()[0] + ":" + str(self.server_socket.getsockname()[1]))
-                conn, addr = self.server_socket.accept()
+    while True:
+      try:
+        print("waiting for connection on " + self.server_socket.getsockname()[0] + ":" + str(
+          self.server_socket.getsockname()[1]))
+        conn, addr = self.server_socket.accept()
 
-                print("accepted command connection: " + addr[0] + ":" + str(addr[1]))
-                fct = ftp_command_thread(conn)
-                fct.start()
-            except:
-                print("Unexpected error: ", sys.exc_info()[0])
-                print("Unexpected error: ", sys.exc_info()[1])
-                print("Unexpected error: ", sys.exc_info()[2])
-                self.server_socket.close()
-                exit()
+        print("accepted command connection: " + addr[0] + ":" + str(addr[1]))
+        fct = ftp_command_thread(conn)
+        fct.start()
+      except:
+        print("Unexpected error: ", sys.exc_info()[0])
+        print("Unexpected error: ", sys.exc_info()[1])
+        print("Unexpected error: ", sys.exc_info()[2])
+        self.server_socket.close()
+        exit()
 
 
 if __name__ == '__main__':
-    # kick it off
-    server = ftp_server()
+  # kick it off
+  server = ftp_server()
