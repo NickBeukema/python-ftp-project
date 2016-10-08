0da0ad24875f8a823c866b69ce23f844458709e0
Jack Rosenhauer II <jackrosenhauer@gmail.com>
2016-10-08 11:39:18 -0400

    fixed a print statement that was throwing an exception

--- ftp-server.py
+++ ftp-server.py
@@ -6,7 +6,8 @@ import pdb
 
 command_port = 7711
 data_port = command_port - 1
-ftp_client_default_port = 7712 #we can't use default ports on the same computer, in a real ftp client the default port for the client is the command_port
+ftp_client_default_port = 7712  # we can't use default ports on the same computer, in a real ftp client the default port for the client is the command_port
+
 
 class ftp_data_thread(threading.Thread):
     def __init__(self, socket, init=False):
@@ -15,7 +16,8 @@ class ftp_data_thread(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-      print ("running data thread")
+        print("running data thread")
+
 
 class ftp_command_thread(threading.Thread):
     def __init__(self, socket):
@@ -28,13 +30,13 @@ class ftp_command_thread(threading.Thread):
         while True:
             self.socket.send("220 awaiting input")
 
-            #wait for commands
+            # wait for commands
             cmd = str(self.socket.recv(1024), "utf-8")
 
             if cmd:
                 if not cmd.endswith("\r\n"):
-                    print ("ERRONEOUS CMD FROM CLIENT")
-                    print (cmd)
+                    print("ERRONEOUS CMD FROM CLIENT")
+                    print(cmd)
                     continue
                 else:
                     cmd = cmd.rstrip("\r\n")
@@ -44,43 +46,43 @@ class ftp_command_thread(threading.Thread):
                 if split[0] == "quit":
                     self.quit()
                 elif split[0] == "list":
-                    print ("list")
+                    print("list")
                     # if len(split) == 2:
                     #     self.list(split[1])
                     # else:
                     #     self.list()
                 elif split[0] == "retr":
-                    print ("retr")
-                #   if len(split) == 2:
+                    print("retr")
+                # if len(split) == 2:
                 #     self.retr(split[1])
                 #   else:
                 #     #error, must supply filename
                 #     print ("retr error no filename")
                 elif split[0] == "stor":
-                    print ("stor")
-                #   if len(split) == 2:
+                    print("stor")
+                # if len(split) == 2:
                 #     self.stor(split[1])
                 #   else:
                 #     print ("stor error no filename")
                 else:
                     print("Unhandled command: " + split[0])
 
-                print ("cmd: " + cmd)
+                print("cmd: " + cmd)
 
     def send_ctrl_response(self, message, encoding="utf-8"):
-      self.socket.sendall(bytearray(message + "\r\n", encoding))
+        self.socket.sendall(bytearray(message + "\r\n", encoding))
 
     def open_data_socket(self):
         if self.data_socket is None:
-          self.send_ctrl_response('150 About to open data connection.')
-          try:
-            self.data_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            self.data_socket.connect(("127.0.0.1", 6548))
-            print("Data connection established.")
-          except:
-            self.send_ctrl_response('425 Can’t open data connection.')
+            self.send_ctrl_response('150 About to open data connection.')
+            try:
+                self.data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.data_socket.connect(("127.0.0.1", 6548))
+                print("Data connection established.")
+            except:
+                self.send_ctrl_response('425 Can’t open data connection.')
         else:
-          self.send_ctrl_response('125 Data connection already open, transfer starting')
+            self.send_ctrl_response('125 Data connection already open, transfer starting')
 
     def close_data_socket(self):
         self.data_socket.close()
@@ -88,71 +90,74 @@ class ftp_command_thread(threading.Thread):
         self.send_ctrl_response('226 Closing data connection.  Requested file action successful.')
 
     def quit(self):
-        print ("quitting")
+        print("quitting")
 
     def list(self):
         # Open data connection
         self.open_data_socket()
 
         try:
-          files_in_dir = os.listdir(self.current_dir)
-          files_in_dir.sort()
-          file_string = ""
-          for file in files_in_dir:
-            file_string = file_string + file + "\n"
-          self.data_socket.sendall(bytearray(file_string + "\r\n", "utf-8"))
+            files_in_dir = os.listdir(self.current_dir)
+            files_in_dir.sort()
+            file_string = ""
+            for file in files_in_dir:
+                file_string = file_string + file + "\n"
+            self.data_socket.sendall(bytearray(file_string + "\r\n", "utf-8"))
         except:
-          self.send_ctrl_response('451 Requested action aborted: local error in processing.')
+            self.send_ctrl_response('451 Requested action aborted: local error in processing.')
 
         self.close_data_socket()
 
     def retr(self, filename):
-      # Open data connection
-      self.openDataConn()
+        # Open data connection
+        self.openDataConn()
 
-      the_file = open(filename, 'rb')
-      data = the_file.read(1024)
-
-      while data:
-        ## TODO: Send the file part
-        ## self.data_sock.send(data)
+        the_file = open(filename, 'rb')
         data = the_file.read(1024)
 
-      ## Can we send it all at once?
-      ## self.data_sock.send(the_file.read())
+        while data:
+            ## TODO: Send the file part
+            ## self.data_sock.send(data)
+            data = the_file.read(1024)
 
-      the_file.close()
-      ## TODO: Close data connection
-      self.socket.send('226 Transfer complete.\r\n')
+        ## Can we send it all at once?
+        ## self.data_sock.send(the_file.read())
+
+        the_file.close()
+        ## TODO: Close data connection
+        self.socket.send('226 Transfer complete.\r\n')
 
     def stor(self, filename):
-        print ("storing file " + filename)
+        print("storing file " + filename)
 
 
 class ftp_server:
-    #basic class/socket setup
+    # basic class/socket setup
     def __init__(self):
-        #self.port = 7711
+        # self.port = 7711
         self.server_socket = socket.socket()
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind(("localhost", command_port))
 
-        #listen for 1 client
+        # listen for 1 client
         self.server_socket.listen(1)
 
         while True:
             try:
-                print ("waiting for connection on " + self.server_socket)
+                print("waiting for connection on " + self.server_socket.getsockname()[0] + ":" + str(self.server_socket.getsockname()[1]))
                 conn, addr = self.server_socket.accept()
 
-                print ("accepted command connection: " + addr[0] + ":" + str(addr[1]))
+                print("accepted command connection: " + addr[0] + ":" + str(addr[1]))
                 fct = ftp_command_thread(conn)
                 fct.start()
             except:
-                print ("Unexpected error: ", sys.exc_info()[0])
+                print("Unexpected error: ", sys.exc_info()[0])
+                print("Unexpected error: ", sys.exc_info()[1])
+                print("Unexpected error: ", sys.exc_info()[2])
                 self.server_socket.close()
                 exit()
 
+
 if __name__ == '__main__':
-  # kick it off
-  server = ftp_server()
+    # kick it off
+    server = ftp_server()
